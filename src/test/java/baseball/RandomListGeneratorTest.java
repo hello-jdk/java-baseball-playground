cbe5f44a39edaae49562b864dd30c8961a8afe83
정동걸 <ehdrjf2062@gmail.com>
2022-11-25 17:30:10 +0900

    test(RandomListGenerator): 랜덤 번호 중복 테스트

--- src/test/java/baseball/RandomListGeneratorTest.java
+++ src/test/java/baseball/RandomListGeneratorTest.java
@@ -1,7 +1,8 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
@@ -19,7 +20,15 @@ class RandomListGeneratorTest {
     void 랜덤번호_범위() {
         List<Integer> list = randomListGenerator.generate();
         long count = list.stream().filter((v) -> v > 0 && v < 10).count();
-        Assertions.assertThat(count).isEqualTo(3);
+        assertThat(count).isEqualTo(3);
     }
 
+    @DisplayName("랜덤 번호는 중복 될 수 없습니다.")
+    @RepeatedTest(100)
+    void 랜덤번호_중복() {
+        List<Integer> list = randomListGenerator.generate();
+        System.out.println(list);
+        long count = list.stream().distinct().count();
+        assertThat(count).isEqualTo(3);
+    }
 }
